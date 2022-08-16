cfb954ef24d9125dbc3e47e932b99faee25c6a42
Yo Kamada <bumpfuji10@gmail.com>
2022-08-16 18:20:31 +0900

    Serviceコンテナ追加

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-use App\Models\Tweet;
+use App\Services\TweetService; // TweetServiceのインポート
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        $tweetService = new TweetService(); //TweetServiceのインスタンスを作成
+        $tweets = $tweetService->getTweets(); //つぶやきの一覧を取得
         return view('tweet.index')
             ->with('tweets', $tweets);
     }
